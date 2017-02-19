ff3f7ea1ababe1895246ac1599336d331d59d97c
JasminBecerra <jbecerra2@wisc.edu>
2017-02-18 18:38:05 -0600

    proportional symbols--initial commit

--- leaflet-lab/js/main.js
+++ leaflet-lab/js/main.js
@@ -6,11 +6,11 @@ function createMap(){
     //create the map linked to div id 'mapid' from index
     var mymap = L.map('mapid', {
         center: [20, 0],
-        zoom: 3
+        zoom: 2
     });
 
     //add base tilelayer (from mapbox + open street map)
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamFzbWluYmVjIiwiYSI6ImNpdXlobG1raDA1MTAybmxnOXRhbTEwaW8ifQ.my00Kwcjtd9QoxJrA3sETA', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
             '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -41,7 +41,7 @@ function getData(mymap){
             };
 
 
-            //add our MegaCitiesMap geoJSON data via a geoJSON layer using the L. method
+            //added geoJSON data via a geoJSON layer using the L. method
             //added optional paramter to make POI symbols circles
             //added onFeature: onEachFeature here, so that I preserved the circle markers
             //and also called the onEachFeature function for population info pop-ups
@@ -53,6 +53,63 @@ function getData(mymap){
         }
     });
 };
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = .0005;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
+};
+
+
+
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data, mymap){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = "YR2015";
+
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#72a393",
+        color: "#4e7265",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            //step 5: for ea. feature, determine value for selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            //step 6: give ea. feature's circle marker a radius based on its att value
+            geojsonMarkerOptions.radius = calcPropRadius(attValue)
+
+            //create the circle markers
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(mymap);
+};
+
+//Step 2: Import GeoJSON data
+function getData(mymap){
+    //load the data
+    $.ajax("data/RefugeeDataMap.geojson", {
+        dataType: "json",
+        success: function(response){
+            //call function to create proportional symbols
+            createPropSymbols(response, mymap);
+        }
+    });
+};
+
 
 
 function onEachFeature(feature, layer) {
