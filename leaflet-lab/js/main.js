84179e774326008faa5c8110ab260c59f5e918de
JasminBecerra <jbecerra2@wisc.edu>
2017-02-19 00:18:14 -0600

    testing pop-ups for proportional symbols

--- leaflet-lab/js/main.js
+++ leaflet-lab/js/main.js
@@ -44,7 +44,7 @@ function getData(mymap){
             //added geoJSON data via a geoJSON layer using the L. method
             //added optional paramter to make POI symbols circles
             //added onFeature: onEachFeature here, so that I preserved the circle markers
-            //and also called the onEachFeature function for population info pop-ups
+            //and also called the onEachFeature function for info pop-ups
             L.geoJson(response, {onEachFeature: onEachFeature,
                 pointToLayer: function (feature, latlng){
                     return L.circleMarker(latlng, geojsonMarkerOptions);
@@ -54,10 +54,13 @@ function getData(mymap){
     });
 };
 
+
+
+
 //calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
     //scale factor to adjust symbol size evenly
-    var scaleFactor = .0005;
+    var scaleFactor = .0009;
     //area based on attribute value and scale factor
     var area = attValue * scaleFactor;
     //radius calculated based on area
@@ -71,7 +74,7 @@ function calcPropRadius(attValue) {
 //Step 3: Add circle markers for point features to the map
 function createPropSymbols(data, mymap){
     //Determine which attribute to visualize with proportional symbols
-    var attribute = "YR2015";
+    var attribute = "YR2012";
 
     //create marker options
     var geojsonMarkerOptions = {
@@ -83,10 +86,10 @@ function createPropSymbols(data, mymap){
         fillOpacity: 0.8
     };
 
-    //create a Leaflet GeoJSON layer and add it to the map
+    //create a Leaflet GeoJSON layer and add to map
     L.geoJson(data, {
         pointToLayer: function (feature, latlng) {
-            //step 5: for ea. feature, determine value for selected attribute
+            //step 5: determine value for selected attribute (for ea. feature)
             var attValue = Number(feature.properties[attribute]);
 
             //step 6: give ea. feature's circle marker a radius based on its att value
@@ -100,7 +103,7 @@ function createPropSymbols(data, mymap){
 
 //Step 2: Import GeoJSON data
 function getData(mymap){
-    //load the data
+    //load data
     $.ajax("data/RefugeeDataMap.geojson", {
         dataType: "json",
         success: function(response){
@@ -113,7 +116,7 @@ function getData(mymap){
 
 
 function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
+    //no property named popupContent; instead, create html string with properties
     var popupContent = "";
     if (feature.properties) {
         //loop to add feature property names and values to html string
@@ -124,7 +127,50 @@ function onEachFeature(feature, layer) {
     };
 };
 
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = "YR2012";
 
+    //create marker options
+    var options = {
+        fillColor: "#72a393",
+        color: "#4e7265",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "<p><b>Country of Origin:</b> " + feature.properties.Country + "</p>";
+
+    //add formatted attribute (year) to popup content string
+    var year = attribute.split("YR");
+    popupContent += "<p><b> Refugees in "+year+":</b> " + feature.properties[attribute] + "</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent);
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, mymap){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: pointToLayer
+    }).addTo(mymap);
+};
 
 $(document).ready(createMap);
 
